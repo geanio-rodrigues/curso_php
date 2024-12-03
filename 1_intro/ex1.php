1623d28ab24f839a684db89627828954c97b15d6
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-12-03 18:17:32 -0300

    sessão 1 concluida

---
+++ 1_intro/ex1.php
@@ -0,0 +1,8 @@
+<?php 
+    $nome = "Geanio";
+    $sobrenome = "Rodrigues";
+    echo "Meu nome é $nome<br>";
+    echo "Meu sobrenome é $sobrenome<br>";
+
+    echo "Nome completo: $nome $sobrenome!<br>";
+?>
