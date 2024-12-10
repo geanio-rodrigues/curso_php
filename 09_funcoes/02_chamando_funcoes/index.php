1c608ed8be622be09f57517f1ae66396ea13281a
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-12-09 21:33:31 -0300

    Update index.php
    Adição de outra função, que funciona também com acentos.

--- 09_funcoes/02_chamando_funcoes/index.php
+++ 09_funcoes/02_chamando_funcoes/index.php
@@ -16,4 +16,10 @@
 
     // função php
     echo strtoupper("testando função");
+    echo "<br>";
+
+    // como a função acima não funciona em acentos, pode ser substituída por outra função
+    $texto = "testando função";
+    echo mb_strtoupper($texto, 'UTF-8');
+    // ou mb_strtoupper("testando função", 'UTF-8');
 ?>
