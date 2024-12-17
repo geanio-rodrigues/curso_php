c6b0aa3196926f1665050340d99abadcf09c5b87
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-12-17 14:57:24 -0300

    Update index.php

--- 10_strings/07_percorrendo_string/index.php
+++ 10_strings/07_percorrendo_string/index.php
@@ -2,11 +2,20 @@
 
     $str = "Esta é uma string muito grande, ela tem vários caracteres";
 
+    // como a frase contém acentas, as letras não serão impressas corretamente
     for($i = 0; $i < strlen($str); $i++){
 
-        echo "$str[$i] <br>";
+        echo "$str[$i] - ";
         
     }
 
-    echo $str;
+    echo "<br>";
+
+    // pode ser usada a função abaixo, para corrigir o erro com os acentos
+
+    mb_internal_encoding('UTF-8'); // Garante que o PHP entenda a codificação correta
+
+    for($i = 0; $i < mb_strlen($str); $i++){
+        echo mb_substr($str, $i, 1) . "<br>";
+    }
 ?>
