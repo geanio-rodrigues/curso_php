194603e3f5be93c7322f0242628ad9e47ac63089
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-01-05 00:45:12 -0300

    separação de templates

---
+++ 15_blog/templates/header.php
@@ -0,0 +1,29 @@
+<?php 
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="<?=$BASER_URL?>/css/styles.css">
+    <!-- GOOGLE FONTES -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="<?=$BASER_URL?>" id="logo">
+            <img src="<?=$BASER_URL?>/img/logo.svg" alt="Blog Codar">
+        </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="<?=$BASER_URL?>">Home</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="<?=$BASER_URL?>contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
