f929ff2321ec5906e4399ba1895e387f2c422ec6
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-01-09 16:31:28 -0300

    Update header.php
    depois de vários testes, tive que mudar o link do bootstrap, e adicionar uma barra a mais no link  do css, pra que pudesse aplicar o código css na página. Antes não estava aplicando algumas funcionalidades.
    
    Ainda não sei o motivo, mas depois dessas modificações, funcionou e está aplicando o css a página corretamente.

--- 17_agenda/templates/header.php
+++ 17_agenda/templates/header.php
@@ -4,8 +4,9 @@
     include_once("config/connection.php");
     include_once("config/process.php");
 
+    // Limpa a mensagem
     if(isset($_SESSION['msg'])) {
-        $printrMsg = $_SESSION['msg'];
+        $printMsg = $_SESSION['msg'];
         $_SESSION['msg'] = '';
     }
 ?>
@@ -17,11 +18,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda de Contatos</title>
     <!-- BOOTSTRAP -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
 </head>
 <body>
     <header>
