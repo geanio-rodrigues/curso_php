d4e08e8ae3550ef4c7b14006178e205010461435
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-02-09 19:41:51 -0300

    Update Movie.php

--- 19_moviestar/models/Movie.php
+++ 19_moviestar/models/Movie.php
@@ -22,10 +22,10 @@
         public function buildMovie($data);
         public function findAll();
         public function getLatestMovies();
-        public function getMoviesByCategory();
-        public function getMoviesByUserId();
-        public function findById();
-        public function findByTitle();
+        public function getMoviesByCategory($category);
+        public function getMoviesByUserId($id);
+        public function findById($id);
+        public function findByTitle($title);
         public function create(Movie $movie);
         public function update(Movie $movie);
         public function destroy($id);
