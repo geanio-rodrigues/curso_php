73e29588b4fbbbe04d42bc359a21226d2cff43ee
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2025-02-08 23:02:07 -0300

    Update header.php

--- 19_moviestar/templates/header.php
+++ 19_moviestar/templates/header.php
@@ -55,7 +55,7 @@
                 <?php if($userData): ?>
                     <li class="nav-item">
                         <a href="<?=$BASE_URL?>newmovie.php" class="nav-link">
-                            <i class="far fa-plus-square"> Incluir Filme</i>
+                            <i class="far fa-plus-square"></i> Incluir Filme
                         </a>
                     </li><li class="nav-item">
                         <a href="<?=$BASE_URL?>dashboard.php" class="nav-link">Meus Filmes</a>
